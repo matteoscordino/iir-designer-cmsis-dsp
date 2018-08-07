0603bdbca3c0d4f983ccc4a768fcf176a571f02c
Matteo Scordino <matteo@silexembedded.co.uk>
2018-08-07 15:14:07 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # iir-designer-cmsis-dsp
-GNU Octave scripts to design IIR filters that can be HW accelerated on ARM Cortex cores via CMSIS DSP
+A collection of GNU Octave scripts to design IIR filters that can be HW accelerated on ARM Cortex cores via CMSIS DSP.
+
+These filter have been written by [Silex Embedded](https://silexembedded.co.uk) and [Elimo Engineering](http://elimo.io)
+
+These scripts compute the coefficients for IIR filters implementented as [biquad cascade using a Direct Form II transposed structure](http://www.keil.com/pack/doc/CMSIS/DSP/html/group__BiquadCascadeDF2T.html "Biquad DF2 documentation").
+This is the IIR implementation that is used by ARM's [CMSIS DSP](http://www.keil.com/pack/doc/CMSIS/DSP/html/index.html "CMSIS DSP' documentation"), which means they can run optimally on ARM Cortex cores over floating point data.
+
+# Usage
+See [the examples file](./iir_designer_usage_examples.txt) for usage examples.
+After running each of those examples, you will get the coefficients array in the format CMSIS expects.
+
+Example 1 will output:
+```
+coeffs =
+
+   0.99799
+  -1.99597
+   0.99799
+   1.99597
+  -0.99598
+```
+
+
+That array of coefficients can be directly used in ARM code like the following:
+```
+#define IIR_ORDER     4
+#define IIR_NUMSTAGES (IIR_ORDER/2)
+
+static float32_t m_biquad_state[IIR_ORDER];
+static float32_t m_biquad_coeffs[5*IIR_NUMSTAGES] =
+{
+  0.0078811,
+  0.0121702,
+  0.0078811,
+  1.8100760,
+  -0.8584909,
+  1.0000000,
+  -1.9999937,
+  1.0000000,
+  1.9564750,
+  -0.9600834
+};
+
+arm_biquad_cascade_df2T_instance_f32 const iir_inst = 
+{
+  IIR_ORDER/2,
+  m_biquad_state,
+  m_biquad_coeffs
+};
+
+extern float32_t* pSrc;
+extern float32_t* pDst;
+extern uint16_t blockSize;
+
+arm_biquad_cascade_df2T_f32(&iir_inst, pSrc, pDst, blockSize);
+
+```
---
+++ design_iir_highpass_cmsis_butter.m
@@ -0,0 +1,60 @@
+% @author Matteo Scordino
+% @date 2018-08-04
+% @version 1.0.0
+% 
+% @brief M file to design a highpass Butterworth filter and get the coefficients for CMSIS DSP
+% 
+
+function design_iir_highpass_cmsis_butter(requested_order, f1, fs, plot_results)
+order = requested_order;
+% Nyquist frequency, for convenience
+fNyquist=fs/2;
+% design filter in zeros, poles, gain format
+[z,p,k] = butter(order, f1/fNyquist, 'high');
+% convert it to the second order sections (used for biquads) format
+[sos] = zp2sos(z,p,k);
+
+% compute biquad coefficients
+coeffs = sos(:,[1 2 3 5 6]);
+% negate a1 and a2 coeffs (CMSIS expects them negated)
+coeffs(:,4) = -coeffs(:,4);
+coeffs(:,5) = -coeffs(:,5);
+
+% make a linear array of it
+coeffs = coeffs';
+coeffs = coeffs(:);
+
+if (plot_results == true)
+	%plot the frequency response, just for human reference
+	% (we need to redesign the filter in b,a format)
+	[b,a] = butter(order,f1/fNyquist, 'high');
+	[h, w] = freqz (b,a);
+	figure(1)
+	plot (w./pi, 20*log10 (abs (h)), ";;")
+	xlabel ("Frequency");
+	ylabel ("abs(H[w])[dB]");
+	axis ([0, 1, -10, 0]);
+	hold ("on");
+	x=ones (1, length (h));
+	hold ("off");
+
+	% plot the filtered result of 3 sample sines
+	data=[[1;zeros(fs-1,1)],[ones(fs,1)],sinetone(f1/2,fs,1,1),sinetone(f1,fs,1,1),sinetone(f1*2,fs,1,1)];
+	filtered = filter(b,a,data);
+	figure(2)
+	clf
+	subplot ( columns ( filtered ), 1, 1)
+	plot(filtered(:,1),";Impulse response;")	
+	subplot ( columns ( filtered ), 1, 2)
+	plot(filtered(:,2),";DC response;")
+	subplot ( columns ( filtered ), 1, 3 )
+	plot(filtered(:,3),";f1/2 Hz response;")
+	subplot ( columns ( filtered ), 1, 4 )
+	plot(filtered(:,4),";f1 response;")
+	subplot ( columns ( filtered ), 1, 5 )
+	plot(filtered(:,5),";f1*2 response;")
+endif
+
+% print the coefficients as expected by CMSIS
+coeffs
+endfunction
